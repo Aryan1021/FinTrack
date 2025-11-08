9cd843e61136249eb93dc1a99827dce3177309ab
Aryan Raj <165568975+Aryan1021@users.noreply.github.com>
2025-11-08 22:11:45 +0530

    refactor URL routing and add class-based view for home

--- finance/urls.py
+++ finance/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from finance.views import home
+from finance.views import home, Homeview
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('home/', home, name='home'),
+    path('', Homeview.as_view(), name='class-home'),
 ]
--- finance/views.py
+++ finance/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, HttpResponse
+from django.views import View
 
-# Create your views here.
 def home(request):
     return HttpResponse("<h1>Hello Django</h1>")
+
+class Homeview(View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("<h1>Hello Django Class</h1>")
