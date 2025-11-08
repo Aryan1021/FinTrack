fa39b11694ec51f8164f696ac64e791fe6e19c50
Aryan Raj <165568975+Aryan1021@users.noreply.github.com>
2025-11-08 23:51:26 +0530

    Implement registration form and view with validation in the finance app

--- finance/views.py
+++ finance/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
 from django.views import View
+from finance.forms import RegisterForm
+from django.contrib.auth import login
 
 class RegisterView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'finance/register.html')
+        form = RegisterForm()
+        return render(request, 'finance/register.html', {'form': form})
+    
+    def post(self, request, *args, **kwargs):
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            redirect('')  
