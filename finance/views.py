f0b1d7c47b82d670356d2cef63feb0dc74d7117a
Aryan Raj <165568975+Aryan1021@users.noreply.github.com>
2025-11-10 22:27:23 +0530

    Add Goal model and related features: create GoalForm, implement GoalCreateView, update admin registration, and enhance templates with goal functionality

--- finance/views.py
+++ finance/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from finance.forms import RegisterForm, TransactionForm
+from finance.forms import RegisterForm, TransactionForm, GoalForm
 from django.contrib.auth import login
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Transaction
+from .models import Transaction, Goal
 
 class RegisterView(View):
     def get(self, request, *args, **kwargs):
@@ -39,3 +39,17 @@ class TransactionListView(LoginRequiredMixin, View):
     def get(self, request, *args, **kwargs):
         transactions = Transaction.objects.all()
         return render(request, 'finance/transaction_list.html', {'transactions': transactions})
+    
+class GoalCreateView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        form = GoalForm()
+        return render(request, 'finance/goal_form.html', {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = GoalForm(request.POST)
+        if form.is_valid():
+            goal = form.save(commit=False)
+            goal.user = request.user
+            goal.save()
+            return redirect('dashboard')
+        return render(request, 'finance/goal_form.html', {'form': form})
